2050f859b1f5de09141fbfde681d84e53257b920
gary909 <garywhite61@hotmail.com>
2023-04-21 13:53:09 +0200

    code working

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 function match(candidate, job) {
     // is this job a valid match for the candidate?
+    // let candidate1 = { minSalary: 120000 },
+    // job1 = { maxSalary: 130000 },
+    // job2 = { maxSalary: 80000 };
+
+    console.log("help!!");
 }
 
 console.log(match(candidate1, job1)); // true
