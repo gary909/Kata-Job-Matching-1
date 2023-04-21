ed02d9ab8263085008303ae0bf393289c07064ea
gary909 <garywhite61@hotmail.com>
2023-04-21 13:15:30 +0200

    code

---
+++ app.js
@@ -0,0 +1,6 @@
+function match(candidate, job) {
+    // is this job a valid match for the candidate?
+}
+
+console.log(match(candidate1, job1)); // true
+console.log(match(candidate1, job2)); // false
